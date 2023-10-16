2ae823828c1d5d47dc83e006d2a414f96088ec0b
Tam Thanh Le (3696) <tamthanhle@kms.com.vn>
2023-10-16 15:08:34 +0700

    uncomment expect timeout

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,11 +10,10 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  // expect: {
-  //   // Maximum time expect() should wait for the condition to be met.
-  //   timeout: 5000,
-
-  // },
+  expect: {
+    // Maximum time expect() should wait for the condition to be met.
+    timeout: 2 * 60 * 1000,
+  },
   testDir: "./tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
