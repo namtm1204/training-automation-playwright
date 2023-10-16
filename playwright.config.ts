094f8dd5dba0c74ab73136d7e6ee0adf3e23eaf2
Tam Thanh Le (3696) <tamthanhle@kms.com.vn>
2023-10-16 10:42:56 +0700

    config report

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,10 +25,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [
-    ["html"],
-    ["allure-playwright", { outputFolder: "allure-results" }],
-  ],
+  reporter: [["allure-playwright", { outputFolder: "allure-results" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
