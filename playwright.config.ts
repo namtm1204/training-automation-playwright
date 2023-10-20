6715b6c526bc1a35fca9226e2b07aab6f09d80a7
Tam Thanh Le (3696) <tamthanhle@kms.com.vn>
2023-10-20 13:31:32 +0700

    config parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: 0,
   /* Opt out of parallel tests on CI. */
-  workers: 3,
+  workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["allure-playwright", { outputFolder: "allure-results" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
