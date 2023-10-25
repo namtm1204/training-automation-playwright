8398d8af01d78a1ddecea77ab27ccaedce1e4b84
Tam Thanh Le (3696) <tamthanhle@kms.com.vn>
2023-10-25 15:50:09 +0700

    update

--- page-objects/abstract-page/abstract-admin-page/AddLocationPage.ts
+++ page-objects/abstract-page/abstract-admin-page/AddLocationPage.ts
@@ -84,6 +84,7 @@ export class AddLocationsPage extends AdminPage {
       await this.clickSave();
 
       await locationsPage.waitForPageLoad();
+      await locationsPage.getAddButton().waitFor({ state: "visible" });
     }
   }
 }
