d5db18480682cd62e2ee4345ff8b98dc6d8f2d5e
Tam Thanh Le (3696) <tamthanhle@kms.com.vn>
2023-12-22 12:22:32 +0700

    update TC09

---
+++ interface/CustomEmployee.ts
@@ -0,0 +1,5 @@
+export interface CustomEmployee {
+  uniqueData: Array<any>;
+  duplicatedData: Array<any>;
+  duplicatedDataIndex: Array<any>;
+}
--- page-objects/abstract-page/abstract-pim-page/PIMConfiguration/DataImportPage.ts
+++ page-objects/abstract-page/abstract-pim-page/PIMConfiguration/DataImportPage.ts
@@ -2,6 +2,7 @@ import { Locator, Page, expect } from "@playwright/test";
 import { PIMPage } from "../PIMPage";
 import path from "path";
 import { Employee } from "../../../../implement/Employee";
+import { CustomEmployee } from "../../../../interface/CustomEmployee";
 
 export class DataImportPage extends PIMPage {
   readonly selectFileButton: Locator;
@@ -66,7 +67,7 @@ export class DataImportPage extends PIMPage {
     );
   }
 
-  async verifyCanShowErrorNotification(customEmployeeData) {
+  async verifyCanShowErrorNotification(customEmployeeData: CustomEmployee) {
     const many1 = customEmployeeData.uniqueData.length > 1 ? "s" : "";
     const many2 = customEmployeeData.duplicatedData.length > 1 ? "s" : "";
     let failedRow = customEmployeeData.duplicatedDataIndex[0];
